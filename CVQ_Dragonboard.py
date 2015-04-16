1920fe14441b366b0dd3cdb90d30177f26eb51e3
Shon Camarlinghi <shoncha.sat@users.noreply.github.com>
2015-04-15 18:12:54 -0700

    Added adb pull of test files to predefined local directory

--- CVQ_Dragonboard.py
+++ CVQ_Dragonboard.py
@@ -10,18 +10,22 @@ import subprocess
 import signal
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 
+#os.system('adb root')
+#time.sleep(2)
+#os.system('adb remount')
+#time.sleep(2)
 adb = os.popen('adb devices').read().strip().split('\n')[1:]
 deviceID = adb[0].split('\t')[0]
 device = MonkeyRunner.waitForConnection(1000, deviceID)
 print 'Hello! :), device under test s/n: %s' % deviceID
 device.shell('logcat -c')
 total_n = 20.0   # number of KWs per case
-total_t = 60*10  # length in seconds per case
-case = ['Clean', 'Car6']  # test cases
-outDir = '/home/ate/workspace/es804_CVQ_QS/English/CVQ' 
+total_t = 620  # length in seconds per case
+case = ['Babble6']  # test cases
+outDir = '/home/ate/workspace/es804_CVQ_QS/English/CVQ'
 FW = '51747'
 VP = 'VPon'
-Delay = '3200ms'
+Delay = '50ms'
 # Uncomment below 3 lines for FRR/FA test
 #KW = 'NihaoZhongXin'
 #fname = os.path.join(outDir, KW + '_CVQ.txt')
@@ -32,7 +36,7 @@ def adbConnection():
         device = MonkeyRunner.waitForConnection(5, deviceID)
         print 'Connected to device: %s' % deviceID
     except IndexError:
-        print 'Could not get adb connection at %s' % time.asctime()
+        print 'Could not get adb connection at %s, is your device off/rebooting?' % time.asctime()
         sys.exit(1)
 
 def exitGracefully(signum, frame):
@@ -42,7 +46,7 @@ def exitGracefully(signum, frame):
 	sys.exit(1)
 
 def asr(ASRres, b):
-    print "ASR"    
+    print "ASR"
     while True:
         logcat1 = device.shell('logcat -d -v time')
         m1 = re.search(r'parsed response', logcat1)
@@ -56,9 +60,22 @@ def asr(ASRres, b):
             break
         else:
             print "Waiting for ASR response..."
-            time.sleep(1)   
- 
-def snooz():    
+            time.sleep(1)
+
+def pull_flac(outDir, value, flacFolder, n):
+	print "Starting pull"
+	#command = ["bash", "./flac.sh", outDir, flac_name, b]
+	#subprocess.call(command, shell=False)
+	os.system(['mkdir', '%s/%s' % (outDir, flacFolder)])
+	flacPath='%s/%s/flac/%s.flac' % (outDir, flacFolder, n)
+	print flacPath
+	combPath='%s/%s/comb/%scomb.pcm' % (outDir, flacFolder, n)
+	print combPath
+	os.system('adb pull /data/data/com.audience.voiceqmultikeyword/files/flacdumpapp.flac %s' % flacPath )
+	os.system('adb pull /data/data/com.audience.voiceqmultikeyword/files/combineddumpapp %s' % combPath )
+	print "Ended pull to %s" % outDir
+
+def snooz():
     device.shell('am force-stop com.android.browser')
     device.touch(460, 900, MonkeyDevice.DOWN_AND_UP)
     #device.press('KEYCODE_BACK', MonkeyDevice.DOWN_AND_UP)
@@ -72,29 +89,35 @@ def snooz():
 		print "CVQ preset set"
 		break
 	else:
-		time.sleep(1)     
+		time.sleep(1)
 
 def frr(value, n, start_t):
-    z =  (total_n-n)/total_n*100            
+    z =  (total_n-n)/total_n*100
     f.write("\nNoise: %s, KW detect count: %d, FRR rate: %.2f" % (value, n, round(z,2)))
     f.write("\nStart time: %s" % (start_t))
     f.write("\nEnd time: %s \n" % (time.asctime()))
-    f.write("==================================================\n\n")     
+    f.write("==================================================\n\n")
 
 def far(value, n, start_t):
     f.write("Noise: %s, KW detect count: %d" % (value, n))
     f.write("\nStart time: %s" % (start_t))
     f.write("\nEnd time: %s \n" % (time.asctime()))
-    f.write("==================================================\n\n") 
+    f.write("==================================================\n\n")
 
 def asrCase(value, ASRres):
-	fname = os.path.join(outDir, value + '_' + Delay + ' _B'+ FW + VP +' _CVQ.txt')
+	fname = os.path.join(outDir, value + '_' + Delay + '_B'+ FW + VP +'_CVQ.txt')
 	f = open(fname, 'w')
 	for i in ASRres:
 		f.write(''.join(str(i)))
 		f.write('\n')
 	f.close()
-                
+def test(outDir, flac_name, b):
+	print "Start"
+	command=["sh", "./test.sh", outDir, flac_name, b ]
+	subprocess.call(command, shell=False)
+	print "End"
+	#subprocess.check_output("echo", "Hello World!")
+
 def KWcount(value):
 	n = 0
 	start_t = time.asctime()
@@ -111,7 +134,12 @@ def KWcount(value):
 				print "KW detection: %d, Lapsed time: %d" % (n, delta_t)
 				b = ' (Utterance_%(n)03d)' % {"n":n}
 				asr(ASRres, b)
-				#comment out above line for FRR/FA test
+				flacFolder = '%s_%s_%s_%s' % (Delay, value, FW, VP)
+				#os.system('adb root')
+                #os.system('adb wait-for-device')
+				pull_flac(outDir, value, flacFolder, n)
+				#comment out above 3 lines for FRR/FA test
+				#test(outDir, flac_name, b)
 				snooz()
 			else:
 				delta_t = time.time() - start
@@ -129,8 +157,8 @@ def main():
 		KWcount(value)
 	#f.close()
     # Uncomment above line for FRR/FA test
-    
+
 if __name__ == '__main__':
 	signal.signal(signal.SIGINT, exitGracefully)
 	main()
-	
+
